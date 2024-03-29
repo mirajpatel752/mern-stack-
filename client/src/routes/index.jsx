4387352fe2ffbda9f5c1d00ff7b63374d9934c34
mirajpatel752 <mirajpatel752@gmail.com>
2024-03-29 23:09:23 +0530

    tailwind css and  auth

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -4,11 +4,16 @@ import { Error } from "../pages/Error";
 import { Register } from "../pages/Register";
 import { Login } from "../pages/Login";
 import MainLayout from "../layouts/index";
+import { AdminLayout } from "../components/layouts/Admin-Layout";
 import { About } from "../pages/About";
 import { Contact } from "../pages/Contact";
 import { Service } from "../pages/Service";
 import { Logout } from "../pages/Logout";
 import { Home } from "../pages/Home";
+import { AdminUsers } from "../pages/Admin-Users";
+import { AdminContacts } from "../pages/Admin-Contacts";
+import { AdminUpdate } from "../pages/Admin-Update";
+import AuthGuard from "../store/AuthGuard";
 import GuestGuard from "../store/GuestGuard";
 
 export default function index() {
@@ -58,6 +63,20 @@ export default function index() {
       ],
     },
 
+    {
+      path: "/admin",
+      element: (
+        <AuthGuard>
+          <AdminLayout />
+        </AuthGuard>
+      ),
+      children: [
+        { element: <Navigate to={"/admin"} replace />, index: true },
+        { path: "users", element: <AdminUsers /> },
+        { path: "contacts", element: <AdminContacts /> },
+        { path: "users/:id/edit", element: <AdminUpdate /> },
+      ],
+    },
 
     { path: "404", element: <Error /> },
     { path: "*", element: <Navigate to="/404" replace /> },
